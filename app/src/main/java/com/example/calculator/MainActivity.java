d9e8e9375a5cd87e1e2ed73fff01c6d54bb80987
Sajeewa Malith <sajeewamalith0219@gmail.com>
2024-02-04 17:37:07 +0530

    when click the buttons,inputs will display in the textview

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,15 +2,163 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+
+     TextView editText;
+     Button bC, bDel, b9, b8, b7, bAdd, b6, b5, b4, bSub, b3, b2, b1, bDiv,
+                   b0, bDot, bEqu, bMul;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText =(TextView) findViewById(R.id.editText);
+        bC = (Button) findViewById(R.id.b_c);
+        bDel = (Button) findViewById(R.id.b_del);
+        b9 = (Button) findViewById(R.id.b_9);
+        b8 = (Button) findViewById(R.id.b_8);
+        b7 = (Button) findViewById(R.id.b_7);
+        bAdd = (Button) findViewById(R.id.b_add);
+        b6 = (Button) findViewById(R.id.b_6);
+        b5 = (Button) findViewById(R.id.b_5);
+        b4 = (Button) findViewById(R.id.b_4);
+        bSub = (Button) findViewById(R.id.b_sub);
+        b3 = (Button) findViewById(R.id.b_3);
+        b2 = (Button) findViewById(R.id.b_2);
+        b1 = (Button) findViewById(R.id.b_1);
+        bDiv = (Button) findViewById(R.id.b_div);
+        b0 = (Button) findViewById(R.id.b_0);
+        bDot = (Button) findViewById(R.id.b_dot);
+        bEqu = (Button) findViewById(R.id.b_equ);
+        bMul = (Button) findViewById(R.id.b_mul);
+
+        bC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText("");
+            }
+        });
+        bDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = editText.getText().toString();
+                if(text.length()>0){
+                    text = text.substring(0,text.length()-1);
+                    editText.setText(text);
+                }
+
+            }
+        });
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"9");
+            }
+        });
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"8");
+            }
+        });
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"7");
+            }
+        });
+        bAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"+");
+            }
+        });
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"6");
+            }
+        });
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"5");
+            }
+        });
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"4");
+            }
+        });
+        bSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"-");
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"3");
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"2");
+            }
+        });
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"1");
+            }
+        });
+        bDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"%");
+            }
+        });
+        b0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"0");
+            }
+        });
+        bDot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+".");
+            }
+        });
+        bEqu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"=");
+            }
+        });
+        bMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText(editText.getText().toString()+"*");
+            }
+        });
+
+
     }
+
+
+
 
 }
